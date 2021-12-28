f436b61abc48f7921de46b7aa23e7511e8c3250b
Ali <modaresi.mr@gmail.com>
2021-12-28 19:29:20 +0100

    docker first version

---
+++ java-agent/docker-run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+MODE=${RUN_MODE:-NO_PRECOMPUTE}
+
+case "$MODE" in
+      NO_PRECOMPUTE)
+        sh launch.sh -all
+		;;
+      PRECOMPUTE)
+        sh launch.sh -allp
+        sh launch.sh -all
+	  *)
+        echo "Unrecognized option: $MODE"
+        exit 1
+        ;;
+esac
